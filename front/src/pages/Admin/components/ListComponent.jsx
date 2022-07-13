a548baaa15844859ce08a6d7a3b53d897151c163
calintha <cal.intha77@gmail.com>
2022-07-13 16:14:36 +0200

    feat: add function table

--- front/src/pages/Admin/components/ListComponent.jsx
+++ front/src/pages/Admin/components/ListComponent.jsx
@@ -9,142 +9,75 @@ import {
 
 function ListComponent( {users = [], messages = [], logs = []}) {
 
-    const bodyTable = ( users, messages, logs ) => {
+    const getProperties = (array) => {
+        if(array !== undefined) {
+            return Object.keys(array)
+        }
+    }
 
-        let body;
+    const getValues = (array) => {
+        if(array !== undefined) {
+            return Object.values(array)
+        }
+    }
+
+    console.log(getValues(users[0]))
+
+    const headTable = ( users, messages, logs ) => {
+
+        let head = []
 
         if(users.length > 0) {
-            body = (
-                users.map( user => (
-                    <tr>
-                        <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200 dark:border-gray-700">
-                            { user.id }
-                        </td>
-                        <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200 dark:border-gray-700">
-                            { user.email }
-                        </td>
-                        <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200 dark:border-gray-700">
-                            { user.fullName }
-                        </td>
-                        <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200 dark:border-gray-700">
-                            {/* <Link to={`/admin/users-list/${user.id}`}>
-                                <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">
-                                    Edit
-                                </button>
-                            </Link> */}
-                        </td>
-                    </tr>
-                ))
+            getProperties(users[0]).map( (property) => {
+                head.push(<th scope="col" class="py-3 px-6">{property}</th>)
+            })
+        }
+        if(messages.length > 0) {
+            getProperties(messages[0]).map( (property) => {
+                head.push(<th scope="col" class="py-3 px-6">{property}</th>)
+            })
+        }
+        if(logs.length > 0) {
+            getProperties(logs[0]).map( (property) => {
+                head.push(<th scope="col" class="py-3 px-6">{property}</th>)
+            })
+        }
+
+        return head;
+    }
+    
+    const bodyTable = ( users, messages, logs ) => {
+
+        let body = [];
+
+        if(users.length > 0) {
+            users.map( (user) => {
+                body.push(<tr>{getValues(user).map( (value) => {
+                    return <td class="border px-6 py-4">{value}</td>
+                })}</tr>)
+            }
             )
         }
         if(messages.length > 0) {
-            body = (
-                messages.map( message => (
-                    <tr>
-                        <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200 dark:border-gray-700">
-                            { message.id }
-                        </td>
-                        <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200 dark:border-gray-700">
-                            { message.sender }
-                        </td>
-                        <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200 dark:border-gray-700">
-                            { message.receiver }
-                        </td>
-                        <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200 dark:border-gray-700">
-                            { message.message }
-                        </td>
-                        <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200 dark:border-gray-700">
-                            { message.date }
-                        </td>
-                    </tr>
-                ))
+            messages.map( (message) => {
+                body.push(<tr>{getValues(message).map( (value) => {
+                    return <td class="border px-6 py-4">{value}</td>
+                })}</tr>)
+            }
             )
         }
         if(logs.length > 0) {
-            body = (
-                logs.map( log => (
-                    <tr>
-                        <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200 dark:border-gray-700">
-                            { log.id }
-                        </td>
-                        <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200 dark:border-gray-700">
-                            { log.date }
-                        </td>
-                        <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200 dark:border-gray-700">
-                            { log.message }
-                        </td>
-                    </tr>
-                ))
+            logs.map( (log) => {
+                body.push(<tr>{getValues(log).map( (value) => {
+                    return <td class="border px-6 py-4">{value}</td>
+                })}</tr>)
+            }
             )
         }
 
         return body;
     }
 
-    const headTable = ( users, messages, logs ) => {
-
-        let head;
-
-        if(users.length > 0) {
-            head = (
-                <tr>
-                    <th scope="col" class="py-3 px-6">
-                        Id
-                    </th>
-                    <th scope="col" class="py-3 px-6">
-                        Email
-                    </th>
-                    <th scope="col" class="py-3 px-6">
-                        FullName
-                    </th>
-                    <th scope="col" class="py-3 px-6">
-                        <span class="sr-only">Edit</span>
-                    </th>
-                </tr>
-            )
-        }
-        if(messages.length > 0) {
-            head = (
-                <tr>
-                    <th scope="col" class="py-3 px-6">
-                        Id
-                    </th>
-                    <th scope="col" class="py-3 px-6">
-                        Sender
-                    </th>
-                    <th scope="col" class="py-3 px-6">
-                        Receiver
-                    </th>
-                    <th scope="col" class="py-3 px-6">
-                        Message
-                    </th>
-                    <th scope="col" class="py-3 px-6">
-                        Date
-                    </th>
-                    <th scope="col" class="py-3 px-6">
-                        <span class="sr-only">Edit</span>
-                    </th>
-                </tr>
-            )
-        }
-        if(logs.length > 0) {
-            head = (
-                <tr>
-                    <th scope="col" class="py-3 px-6">
-                        Id
-                    </th>
-                    <th scope="col" class="py-3 px-6">
-                        Date
-                    </th>
-                    <th scope="col" class="py-3 px-6">
-                        Message
-                    </th>
-                </tr>
-            )
-        }
-
-        return head;
-    }
     return (
         <div class="overflow-x-auto relative shadow-md sm:rounded-lg mx-5 my-5">
             <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
