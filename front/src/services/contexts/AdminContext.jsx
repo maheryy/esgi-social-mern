2e0704de6ebd06cdce18cbacdcb78ced2f487dca
Sadik Raida <sadik.raida@gmail.com>
2022-07-24 23:07:14 +0200

    feat: add context admin

--- front/src/services/contexts/AdminContext.jsx
+++ front/src/services/contexts/AdminContext.jsx
@@ -4,16 +4,24 @@ export const AdminContext = createContext();
 
 const AdminProvider = ({ children }) => {
 
-  const [loggedUser, setLoggedUser]= useState(null);
-  const [token, setToken] = useState("");
+  const {loggedUser, token} = useAuthContext();
+  const navigate = useNavigate();
+
+
+  useEffect(()=> {
+    if(JSON.parse(localStorage.getItem('userInfo')) == null){
+      navigate('/login', {replace:true});
+    }else{
+      navigate('/admin', {replace: true});
+    }
+  },
+  [])
 
   return (
     <AdminContext.Provider
       value={{
-        loggedUser, 
-        setLoggedUser,
+        loggedUser,        
         token,
-        setToken
       }}
     >
       {children}
