408fade83611a8e6bc83a9b31e7bdf6224bc3f79
Alexis LOURS <alexislours@protonmail.com>
2022-07-24 19:17:31 +0200

    feat: add client errors table

--- front/src/pages/Admin/LogsList.jsx
+++ front/src/pages/Admin/LogsList.jsx
@@ -6,15 +6,28 @@ import { API_URL } from "../../services/constants/index.js";
 function LogsList() {
 
     const [logs, setLogs] = useState(false)
+    const [clientErrors, setClientErrors] = useState(false)
 
     useEffect(() => { fetchLogs() }, [])
+    useEffect(() => { fetchClientErrors() }, [])
 
     const fetchLogs = useCallback(() => {
-        fetch(`${API_URL}/access-logs?perPage=100`)
+        fetch(`${API_URL}/access-logs?perPage=20`)
             .then((res) => res.json())
             .then((res) => {
                 console.log(res)
                 setLogs(res)
+            })
+            .catch((error) => {
+                console.error(error);
+            });
+    }, [])
+    const fetchClientErrors = useCallback(() => {
+        fetch(`${API_URL}/client-errors?perPage=100`)
+            .then((res) => res.json())
+            .then((res) => {
+                console.log(res)
+                setClientErrors(res)
             })
             .catch((error) => {
                 console.error(error);
@@ -26,7 +39,8 @@ function LogsList() {
             <div className="text-left overflow-x-auto relative mx-5 my-5">
                 <h2 className="font-bold">Liste des logs</h2>
             </div>
-            <div>
+            <div className="text-left overflow-x-auto relative mx-5 my-5">
+                <p>Logs API:</p>
                 <table>
                     <thead>
                         <tr>
@@ -49,6 +63,32 @@ function LogsList() {
                                         <td>{log.res.statusCode}</td>
                                         <td>{log.req.url}</td>
                                         <td>{log.req?.body ? JSON.stringify(log.req.body, null, 2) :''}</td>
+                                    </tr>
+                                )
+                            })
+                        }
+                    </tbody>
+                </table>
+
+                <p>Logs Client:</p>
+                <table>
+                    <thead>
+                        <tr>
+                            <th>Erreur</th>
+                            <th>Date</th>
+                            <th>Message</th>
+                            <th>Stack</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {clientErrors.length > 0 &&
+                            clientErrors.map((log) => {
+                                return (
+                                    <tr>
+                                        <td>{log._id}</td>
+                                        <td>{log.timestamp}</td>
+                                        <td>{log.message}</td>
+                                        <td>{log.stack}</td>
                                     </tr>
                                 )
                             })
